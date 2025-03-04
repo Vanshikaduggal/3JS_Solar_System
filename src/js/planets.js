ab8907341c1ddcf582068110d501190b1bcf20cf
Vanshika <vanshikaduggal13@gmail.com>
2025-03-04 16:14:04 +0530

    first commit

--- src/js/planets.js
+++ src/js/planets.js
@@ -10,94 +10,92 @@ import neptune from '../assets/imgs/neptune.jpg';
 import pluto from '../assets/imgs/pluto.jpg';
 
 export default planets = {
-		"sun": {
-			"Type": "Star",
-			"Diameter": "1391000 Km",
-			"Mass": "1.989e30 Kg",
-			"Notable Feature": "Central gravitational force that governs the motion of the planets in our solar system.",
-			"img": sun,
-
-		},
-		"mercury": {
-			"Type": "Planet",
-			"Distance(to Sun)": "57.9 million km",
-			"Orbital Period": "88 days",
-			"Diameter": "4880 Km",
-			"Mass": "3.285e23 Kg",
-			"Notable Feature": "Extreme temperature variations, ranging from extremely hot to extremely cold.",
-			"img": mercury
-		},
-		"venus": {
-			"Type": "Planet",
-			"Distance(to Sun)": "108.2 million km",
-			"Orbital Period": "225 days",
-			"Diameter": "12104 Km",
-			"Mass": "4.867e24 Kg",
-			"Notable Feature": "Thick atmosphere composed mostly of carbon dioxide, causing a strong greenhouse effect.",
-			"img": venus
-		},
-		"earth": {
-			"Type": "Planet",
-			"Distance(to Sun)": "149.6 million km",
-			"Orbital Period": "365.25 days",
-			"Diameter": "12742 Km",
-			"Mass": "5.972e24 Kg",
-			"Notable Feature": "Supports life, unique with liquid water on the surface.",
-			"img": earth
-		},
-		"mars": {
-			"Type": "Planet",
-			"Distance(to Sun)": "227.9 million km",
-			"Orbital Period": "687 days",
-			"Diameter": "6779 Km",
-			"Mass": "6.417e23 Kg",
-			"Notable Feature": "Reddish appearance due to iron oxide on its surface. Has polar ice caps and the tallest volcano, Olympus Mons.",
-			"img": mars
-		},
-		"jupiter": {
-			"Type": "Planet",
-			"Distance(to Sun)": "778.3 million km",
-			"Orbital Period": "11.9 years",
-			"Diameter": "139820 Km",
-			"Mass": "1.898e27 Kg",
-			"Notable Feature": "Reddish appearance due to iron oxide on its surface. Has polar ice caps and the tallest volcano, Olympus Mons.",
-			"img": jupiter
-		},
-		"saturn": {
-			"Type": "Planet",
-			"Distance(to Sun)": "1.4 billion km",
-			"Orbital Period": "29.5 years",
-			"Diameter": "116460 Km",
-			"Mass": "5.683e26 Kg",
-			"Notable Feature": "Known for its prominent ring system, made up of ice particles and dust.",
-			"img": saturn
-		},
-		"uranus": {
-			"Type": "Planet",
-			"Distance(to Sun)": "2.9 billion km",
-			"Orbital Period": "84 years",
-			"Diameter": "50724 Km",
-			"Mass": "8.681e25 Kg",
-			"Notable Feature": "Rotates on its side, likely due to a past collision. Has a blue-green color due to methane in its atmosphere.",
-			"img": uranus
-		},
-		"neptune": {
-			"Type": "Planet",
-			"Distance(to Sun)": "4.5 billion km",
-			"Orbital Period": "164.8 years",
-			"Diameter": "49244 Km",
-			"Mass": "1.024e26 Kg",
-			"Notable Feature": "Strong winds and the presence of the Great Dark Spot, a massive storm system.",
-			"img": neptune
-		},
-		"pluto": {
-			"Type": "Planet",
-			"Distance(to Sun)": "Varies (dwarf planet in the Kuiper Belt)",
-			"Orbital Period": "248 years",
-			"Diameter": "2377 Km",
-			"Mass": "1.31e22 Kg",
-			"Notable Feature": " Classified as a dwarf planet. Has a thin atmosphere and five known moons, including Charon.",
-			"img": pluto
-		}
-		
-	}
+    "sun": {
+        "Type": "Star",
+        "Diameter": "1,391,000 Km",
+        "Mass": "1.989e30 Kg",
+        "Notable Feature": "Central gravitational force that governs the motion of the planets and other celestial objects in the solar system.",
+        "img": sun,
+    },
+    "mercury": {
+        "Type": "Planet",
+        "Distance(to Sun)": "57.9 million km",
+        "Orbital Period": "88 days",
+        "Diameter": "4,880 Km",
+        "Mass": "3.285e23 Kg",
+        "Notable Feature": "Extreme temperature variations, with a surface temperature ranging from about -180°C at night to 430°C during the day.",
+        "img": mercury
+    },
+    "venus": {
+        "Type": "Planet",
+        "Distance(to Sun)": "108.2 million km",
+        "Orbital Period": "225 days",
+        "Diameter": "12,104 Km",
+        "Mass": "4.867e24 Kg",
+        "Notable Feature": "Venus has a thick atmosphere mainly composed of carbon dioxide, resulting in a surface temperature hot enough to melt lead.",
+        "img": venus
+    },
+    "earth": {
+        "Type": "Planet",
+        "Distance(to Sun)": "149.6 million km",
+        "Orbital Period": "365.25 days",
+        "Diameter": "12,742 Km",
+        "Mass": "5.972e24 Kg",
+        "Notable Feature": "The only known planet to support life, with abundant liquid water and a diverse range of ecosystems.",
+        "img": earth
+    },
+    "mars": {
+        "Type": "Planet",
+        "Distance(to Sun)": "227.9 million km",
+        "Orbital Period": "687 days",
+        "Diameter": "6,779 Km",
+        "Mass": "6.417e23 Kg",
+        "Notable Feature": "Known for its reddish appearance due to iron oxide (rust) on its surface. Mars also has the largest volcano in the solar system, Olympus Mons.",
+        "img": mars
+    },
+    "jupiter": {
+        "Type": "Planet",
+        "Distance(to Sun)": "778.3 million km",
+        "Orbital Period": "11.9 years",
+        "Diameter": "139,820 Km",
+        "Mass": "1.898e27 Kg",
+        "Notable Feature": "Jupiter is the largest planet in the solar system, known for its Great Red Spot, a massive storm that has raged for centuries.",
+        "img": jupiter
+    },
+    "saturn": {
+        "Type": "Planet",
+        "Distance(to Sun)": "1.4 billion km",
+        "Orbital Period": "29.5 years",
+        "Diameter": "116,460 Km",
+        "Mass": "5.683e26 Kg",
+        "Notable Feature": "Famous for its stunning ring system made of ice particles, dust, and rocky debris.",
+        "img": saturn
+    },
+    "uranus": {
+        "Type": "Planet",
+        "Distance(to Sun)": "2.9 billion km",
+        "Orbital Period": "84 years",
+        "Diameter": "50,724 Km",
+        "Mass": "8.681e25 Kg",
+        "Notable Feature": "Rotates on its side, likely due to a past collision with an Earth-sized object. Its atmosphere contains methane, giving it a blue-green color.",
+        "img": uranus
+    },
+    "neptune": {
+        "Type": "Planet",
+        "Distance(to Sun)": "4.5 billion km",
+        "Orbital Period": "164.8 years",
+        "Diameter": "49,244 Km",
+        "Mass": "1.024e26 Kg",
+        "Notable Feature": "Neptune is known for its strong winds, the fastest in the solar system, and the Great Dark Spot, a massive storm system.",
+        "img": neptune
+    },
+    "pluto": {
+        "Type": "Dwarf Planet",
+        "Distance(to Sun)": "Varies (located in the Kuiper Belt)",
+        "Orbital Period": "248 years",
+        "Diameter": "2,377 Km",
+        "Mass": "1.31e22 Kg",
+        "Notable Feature": "Pluto, classified as a dwarf planet, has a thin atmosphere that expands and contracts depending on its position relative to the Sun. It has five known moons, including Charon.",
+        "img": pluto
+    }
+}
